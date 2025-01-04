662ed40cfd382226ae4625bd2f35969cd3978189
Shish <shish@shishnet.org>
2025-01-04 16:40:15 +0000

    more

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ -z "$1" ]; then
-  echo "Usage: $0 <package name>"
+if [ ! -d "$1" ]; then
+  echo "Usage: $0 <package directory>"
   exit 1
 fi
 PKG=$1
 docker build $PKG
-docker run --rm $(docker build -q $PKG) cat $PKG.deb > $PKG.deb
+docker run --rm $(docker build -q $PKG) cat $(basename $PKG).deb > $(basename $PKG).deb
